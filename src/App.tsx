224078f5b5abcd5faf4e91fc8cc0ab986bc7d4f2
andrwyoung <yong.andrew11@gmail.com>
2025-08-04 20:12:32 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex flex-col gap-2 items-center  justify-center bg-white">
+      <h1 className="text-4xl font-header font-normal">Color Puzzle Game!</h1>
+
+      <p className="font-body">hey there, this is a game game game</p>
+    </div>
+  );
+}
