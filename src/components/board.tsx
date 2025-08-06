138db020134a5c5110ffb72eeb7ff47b76a438b2
andrwyoung <yong.andrew11@gmail.com>
2025-08-06 10:47:09 -0700

    render the pieces

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,30 +1,20 @@
+// this is the board, as well as the pieces that are not on the board yet
+
 import { useState } from "react";
 import { BOARD_COLS, BOARD_ROWS } from "../lib/constants/board.ts";
 import { getPieceColor } from "../lib/ui-helpers/get-piece-color";
 import { CELL_SIZE } from "../lib/constants/ui";
+import RandomizeButton from "./ui-components/randomize-button.tsx";
+import PieceContainer from "./piece-container.tsx";
 
 export default function Board() {
   const [currentBoard, setCurrentBoard] = useState<(number | null)[][]>(() =>
     Array.from({ length: BOARD_ROWS }, () => Array(BOARD_COLS).fill(null))
   );
 
-  const randomizeBoard = () => {
-    setCurrentBoard(() =>
-      Array.from({ length: BOARD_ROWS }, () =>
-        Array.from({ length: BOARD_COLS }, () => Math.floor(Math.random() * 12) + 1)
-      )
-    );
-  };
-
   return (
     <>
-      <button
-        onClick={randomizeBoard}
-        className="mb-2 px-4 py-2 text-lg font-header bg-emerald-400 text-white rounded 
-        hover:bg-emerald-300 cursor-pointer"
-      >
-        Random Colors!!!!
-      </button>
+      <RandomizeButton setBoard={setCurrentBoard} />
       <div className="grid grid-rows-5 grid-cols-11 w-fit bg-emerald-300 rounded-lg p-1">
         {currentBoard.map((row, rowIndex) =>
           row.map((cell, colIndex) => (
@@ -42,6 +32,8 @@ export default function Board() {
           ))
         )}
       </div>
+
+      <PieceContainer />
     </>
   );
 }
---
+++ src/components/ui-components/randomize-button.tsx
@@ -0,0 +1,26 @@
+import type { Dispatch, SetStateAction } from "react";
+import { BOARD_COLS, BOARD_ROWS } from "../../lib/constants/board";
+
+type Props = {
+  setBoard: Dispatch<SetStateAction<(number | null)[][]>>;
+};
+
+export default function RandomizeButton({ setBoard }: Props) {
+  const randomizeBoard = () => {
+    setBoard(() =>
+      Array.from({ length: BOARD_ROWS }, () =>
+        Array.from({ length: BOARD_COLS }, () => Math.floor(Math.random() * 12) + 1)
+      )
+    );
+  };
+
+  return (
+    <button
+      onClick={randomizeBoard}
+      className="mb-2 px-4 py-2 text-lg font-header bg-emerald-400 text-white rounded 
+      hover:bg-emerald-300 cursor-pointer"
+    >
+      Random Colors!!!!
+    </button>
+  );
+}
