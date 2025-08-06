eb567beca14dba1d02963203c5f52c7b09c655b6
andrwyoung <yong.andrew11@gmail.com>
2025-08-05 20:51:07 -0700

    primative board

---
+++ src/components/piece.tsx
@@ -0,0 +1,34 @@
+import { CELL_SIZE } from "../lib/constants/ui";
+import { type Coordinate } from "../types/board";
+
+export function Piece({
+  variation,
+  anchor,
+  color,
+}: {
+  variation: Coordinate[];
+  anchor: [number, number];
+  color: string;
+}) {
+  return (
+    <div className="absolute">
+      {variation.map(([dr, dc], i) => {
+        const top = (anchor[0] + dr) * CELL_SIZE;
+        const left = (anchor[1] + dc) * CELL_SIZE;
+        return (
+          <div
+            key={i}
+            className="absolute"
+            style={{
+              top,
+              left,
+              width: CELL_SIZE,
+              height: CELL_SIZE,
+              backgroundColor: color,
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+}
